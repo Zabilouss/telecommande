cec91447766fab4eb3812391d959fd0af305f2c7
Zabilouss <basile.bulteau@gmail.com>
2025-01-14 19:09:10 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,12 +16,24 @@ makerbit.onIrButton(IrButton.Number_5, IrButtonAction.Pressed, function () {
     pins.analogWritePin(AnalogPin.P13, 1023)
     pins.analogWritePin(AnalogPin.P15, 1023)
 })
+function Gauche () {
+    pins.analogWritePin(AnalogPin.P13, 0)
+    pins.analogWritePin(AnalogPin.P15, 0)
+    pins.analogWritePin(AnalogPin.P14, 1023)
+    pins.analogWritePin(AnalogPin.P16, 0)
+}
 makerbit.onIrButton(IrButton.Any, IrButtonAction.Released, function () {
     pins.analogWritePin(AnalogPin.P14, 0)
     pins.analogWritePin(AnalogPin.P16, 0)
     pins.analogWritePin(AnalogPin.P13, 0)
     pins.analogWritePin(AnalogPin.P15, 0)
 })
+function Reculer () {
+    pins.analogWritePin(AnalogPin.P13, 0)
+    pins.analogWritePin(AnalogPin.P15, 0)
+    pins.analogWritePin(AnalogPin.P13, 1023)
+    pins.analogWritePin(AnalogPin.P15, 1023)
+}
 makerbit.onIrButton(IrButton.Down, IrButtonAction.Pressed, function () {
     pins.analogWritePin(AnalogPin.P13, 0)
     pins.analogWritePin(AnalogPin.P15, 0)
@@ -33,6 +45,12 @@ function Avancer () {
     pins.analogWritePin(AnalogPin.P15, 0)
     pins.analogWritePin(AnalogPin.P14, 1023)
     pins.analogWritePin(AnalogPin.P16, 1023)
+}
+function Droite () {
+    pins.analogWritePin(AnalogPin.P13, 0)
+    pins.analogWritePin(AnalogPin.P15, 0)
+    pins.analogWritePin(AnalogPin.P14, 1023)
+    pins.analogWritePin(AnalogPin.P16, 0)
 }
 led.enable(false)
 basic.forever(function () {
