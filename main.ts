8d41968942f039592000d823cd6f9ebf6409f79d
Zabilouss <basile.bulteau@gmail.com>
2025-01-14 19:05:32 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,40 @@
+makerbit.onIrButton(IrButton.Number_3, IrButtonAction.Pressed, function () {
+    pins.analogWritePin(AnalogPin.P13, 0)
+    pins.analogWritePin(AnalogPin.P15, 0)
+    pins.analogWritePin(AnalogPin.P14, 1023)
+    pins.analogWritePin(AnalogPin.P16, 0)
+})
+makerbit.onIrButton(IrButton.Number_1, IrButtonAction.Pressed, function () {
+    pins.analogWritePin(AnalogPin.P13, 0)
+    pins.analogWritePin(AnalogPin.P15, 0)
+    pins.analogWritePin(AnalogPin.P14, 1023)
+    pins.analogWritePin(AnalogPin.P16, 0)
+})
+makerbit.onIrButton(IrButton.Number_5, IrButtonAction.Pressed, function () {
+    pins.analogWritePin(AnalogPin.P13, 0)
+    pins.analogWritePin(AnalogPin.P15, 0)
+    pins.analogWritePin(AnalogPin.P13, 1023)
+    pins.analogWritePin(AnalogPin.P15, 1023)
+})
 makerbit.onIrButton(IrButton.Any, IrButtonAction.Released, function () {
     pins.analogWritePin(AnalogPin.P14, 0)
     pins.analogWritePin(AnalogPin.P16, 0)
     pins.analogWritePin(AnalogPin.P13, 0)
     pins.analogWritePin(AnalogPin.P15, 0)
 })
-makerbit.onIrButton(IrButton.Any, IrButtonAction.Pressed, function () {
+makerbit.onIrButton(IrButton.Down, IrButtonAction.Pressed, function () {
     pins.analogWritePin(AnalogPin.P13, 0)
     pins.analogWritePin(AnalogPin.P15, 0)
     pins.analogWritePin(AnalogPin.P14, 1023)
     pins.analogWritePin(AnalogPin.P16, 1023)
 })
-led.enable(false)
-for (let index = 0; index < 1000; index++) {
-    basic.pause(200)
-    makerbit.connectIrReceiver(DigitalPin.P7, IrProtocol.NEC)
+function Avancer () {
+    pins.analogWritePin(AnalogPin.P13, 0)
+    pins.analogWritePin(AnalogPin.P15, 0)
+    pins.analogWritePin(AnalogPin.P14, 1023)
+    pins.analogWritePin(AnalogPin.P16, 1023)
 }
+led.enable(false)
+basic.forever(function () {
+    makerbit.connectIrReceiver(DigitalPin.P7, IrProtocol.NEC)
+})
